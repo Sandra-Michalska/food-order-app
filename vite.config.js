2ec24be8daa1bd2d59b9853464c1649f34ea011d
Sandra Michalska <sandra.kinga.michalska@gmail.com>
2023-12-06 12:18:12 +0100

    Tab size improvement

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  return {
-    plugins: [react()],
-    base: mode === "production"
-      ? '/food-order-app/'
-      : '/'
-  };
+    return {
+        plugins: [react()],
+        base: mode === "production"
+            ? '/food-order-app/'
+            : '/'
+    };
 });
